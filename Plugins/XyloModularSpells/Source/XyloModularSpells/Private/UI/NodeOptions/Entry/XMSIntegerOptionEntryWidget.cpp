5ae1bc1a285fcc211301c0ddf0e036d77a591a89
XyloIsCoding <xylo.is.coding@gmail.com>
2025-07-08 20:12:46 +0200

    added integer option entry

---
+++ Plugins/XyloModularSpells/Source/XyloModularSpells/Private/UI/NodeOptions/Entry/XMSIntegerOptionEntryWidget.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/NodeOptions/Entry/XMSIntegerOptionEntryWidget.h"
+
+#include "UI/BaseWidget/XMSNodeTextWidget.h"
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * UUserWidget Interface
+ */
+
+void UXMSIntegerOptionEntryWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (OptionText)
+	{
+		OptionText->NodeTextClickedDelegate.AddUObject(this, &UXMSIntegerOptionEntryWidget::BroadcastOptionSelectedDelegate);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * UXMSNodeOptionEntryWidget Interface
+ */
+
+void UXMSIntegerOptionEntryWidget::ClearDelegates()
+{
+	Super::ClearDelegates();
+	IntegerOptionSelectedDelegate.Clear();
+}
+
+void UXMSIntegerOptionEntryWidget::InitializeOption(int32 InOptionIndex)
+{
+	Super::InitializeOption(InOptionIndex);
+}
+
+void UXMSIntegerOptionEntryWidget::BroadcastOptionSelectedDelegate()
+{
+	IntegerOptionSelectedDelegate.Broadcast(OptionValue);
+	Super::BroadcastOptionSelectedDelegate();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * UXMSIntegerOptionEntryWidget
+ */
+
+void UXMSIntegerOptionEntryWidget::SetOptionValue(int32 InValue, const FString& DisplayOverride)
+{
+	OptionValue = InValue;
+
+	OptionText->SetDisplayText(!DisplayOverride.IsEmpty() ? DisplayOverride : FString::FromInt(OptionValue));
+}
